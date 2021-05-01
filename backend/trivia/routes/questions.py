81af0e9a6d22c68d2abe3865c1a7085b35d78e19
myportfolio-tech <eduardo@myportfolio.com>
2021-05-01 16:20:34 -0400

    Saturday afternoon preparation

--- backend/trivia/routes/questions.py
+++ backend/trivia/routes/questions.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, render_template, request, redirect, url_for
-
+from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from trivia.extensions import db 
+from trivia.models import Question
 
 question = Blueprint('question', __name__)
 
@@ -7,6 +8,6 @@ question = Blueprint('question', __name__)
 @question.route('/questions', methods=['GET'])
 def get_questions():
 
-    return """ 
-    <h1>THESE ARE THE QUESTIONS</h1>"""
-
+    questions = Question.query.all()
+    
+    return jsonify(questions)
