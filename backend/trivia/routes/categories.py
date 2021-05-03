6ac17c1a28c1bd41b09b789441161c0863a00b25
myportfolio-tech <eduardo@myportfolio.com>
2021-05-03 16:32:06 -0400

    finished the '/categories/<int:id>/questions' route

--- backend/trivia/routes/categories.py
+++ backend/trivia/routes/categories.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, jsonify
+from flask import Blueprint, request, jsonify, abort
 from trivia.extensions import db 
 from trivia.models import Category, Question
 
@@ -20,4 +20,26 @@ def get_categories():
     
     return jsonify({
           'categories': categories_dict
+      })
+
+
+@category.route('/categories/<int:id>/questions', methods=['GET'])
+def get_questions_per_category(id):
+
+    page = request.args.get('page', 1, type=int)
+    start = (page-1) * 10
+    end = start + 10
+
+    category = Category.query.filter_by(id=id).one_or_none()
+
+    if category is None:
+        abort(400)
+        
+    questions = Question.query.filter_by(category=category.id).all()
+    formatted_questions = [question.format() for question in questions]
+
+    return jsonify({
+          'questions': formatted_questions[start:end],
+            'total_questions': len(formatted_questions),
+          'currentCategory': category.type
       })
